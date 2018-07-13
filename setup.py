c7db23f8d02c4ef782c126d7f02617d44e2a485e
Maikel <maikelboogerd@gmail.com>
2018-07-13 22:46:53 +0200

    Fix import and git url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     description='Produce and consume events with Kafka.',
     author='Maikel van den Boogerd',
     author_email='maikelboogerd@gmail.com',
-    url='https://github.com/maikelboogerd/python-eventcore',
+    url='https://github.com/maikelboogerd/eventcore-kafka',
     keywords=['event', 'kafka', 'producer', 'consumer'],
     packages=['eventcore_kafka'],
     install_requires=['eventcore', 'confluent-kafka'],
