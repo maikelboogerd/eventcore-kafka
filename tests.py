de5bb65656695550495dfd42d8dd0bf319ae8e32
CasperCL <caspereekhof@icloud.com>
2018-11-12 09:59:22 +0100

    Add unittest for BlockingKafkaConsumer

--- tests.py
+++ tests.py
@@ -1,8 +1,12 @@
 import unittest
 from collections import namedtuple
-from unittest.mock import Mock
+from unittest.mock import Mock, patch, MagicMock
 
-from eventcore_kafka import KafkaConsumer
+from confluent_kafka.cimpl import KafkaException
+
+from eventcore_kafka import KafkaConsumer, BlockingKafkaConsumer
+
+from eventcore.exceptions import FatalConsumerError
 
 
 def consumer_settings():
@@ -20,8 +24,7 @@ def mock_message() -> Message:
     return Message(
         key=Mock(return_value=b"MyKey"),
         value=Mock(return_value=b'{"data": []}'),
-        error=Mock(return_value=False)
-    )
+        error=Mock(return_value=False))
 
 
 def mock_handler(name, subject, data):
@@ -29,53 +32,71 @@ def mock_handler(name, subject, data):
 
 
 class KafkaConsumerTest(unittest.TestCase):
-    def test_auto_commit_enabled(self):
-        """When auto_commit is enabled on the server, it shouldn't commit."""
+    def test_is_valid_message(self):
+        message = mock_message()
+        valid = KafkaConsumer.is_valid_message(message)
+
+        assert valid
+
+    def test_is_invalid_message_error(self):
+        error = Mock()
+        message = Message(
+            key=Mock(return_value=b"MyKey"),
+            value=Mock(return_value=b'{"data": []}'),
+            error=Mock(return_value=error))
+        self.assertRaises(
+            KafkaException, KafkaConsumer.is_valid_message, message=message)
+
+    def test_is_invalid_message(self):
+        valid = KafkaConsumer.is_valid_message(b"")
+        assert valid is False
+
+    def test_parse_message(self):
+        message = mock_message()
+        subject, message_body = KafkaConsumer.parse_message(message)
+        assert subject == "MyKey"
+        assert len(message_body['data']) == 0
+
+
+class ConsumerMock(Mock):
+    pass
+
+
+class BlockingKafkaConsumerTest(unittest.TestCase):
+    @patch("eventcore_kafka.consumer.kafka")
+    def test_commit_message(self, kafka):
+        message = mock_message()
+
+        self._patch_consumer(kafka, message)
+
         settings = consumer_settings()
-        consumer = KafkaConsumer(**settings)
-        kafka_consumer = Mock()
-        kafka_consumer.poll = Mock(return_value=mock_message())
-        kafka_consumer.commit = Mock()
+        consumer = BlockingKafkaConsumer(**settings)
+        consumer.process_event = Mock()
 
-        consumer.process_event = mock_handler
-        consumer.kafka_consumer = kafka_consumer
+        consumer.poll_and_process()
 
-        with self.assertRaises(NotImplementedError):
-            consumer.poll_and_process()
+        assert kafka.Consumer.commit.called is True
 
-        assert kafka_consumer.poll.called
-        assert kafka_consumer.commit.called is False
+    @patch("eventcore_kafka.consumer.kafka")
+    def test_blocking_message(self, kafka):
+        message = mock_message()
 
-    def test_auto_commit_disabled(self):
-        """When auto_commit is disabled on the server, it should commit."""
-        consumer = KafkaConsumer(**consumer_settings())
-        kafka_consumer = Mock()
-        kafka_consumer.poll = Mock(return_value=mock_message())
-        kafka_consumer.commit = Mock()
+        self._patch_consumer(kafka, message)
 
-        consumer.process_event = mock_handler
-        consumer.kafka_consumer = kafka_consumer
-
-        with self.assertRaises(NotImplementedError):
-            consumer.poll_and_process()
-
-        assert kafka_consumer.poll.called
-        assert kafka_consumer.commit.called
-
-    def test_consume_commit_after_processing(self):
-        """When server_auto_commit disabled don't commit a failed handler."""
         settings = consumer_settings()
-        settings["commit_after_processing"] = True
-        consumer = KafkaConsumer(**settings)
-        kafka_consumer = Mock()
-        kafka_consumer.poll = Mock(return_value=mock_message())
-        kafka_consumer.commit = Mock()
-
+        consumer = BlockingKafkaConsumer(**settings)
         consumer.process_event = mock_handler
-        consumer.kafka_consumer = kafka_consumer
 
-        with self.assertRaises(NotImplementedError):
+        with self.assertRaises(FatalConsumerError):
             consumer.poll_and_process()
 
-        assert kafka_consumer.poll.called
-        assert kafka_consumer.commit.called is False
+        assert kafka.Consumer.commit.called is False
+
+    @staticmethod
+    def _patch_consumer(kafka, message):
+        ConsumerMock.poll = Mock(return_value=message)
+        ConsumerMock.subscribe = Mock()
+        ConsumerMock.commit = Mock()
+        kafka.KafkaError._PARTITION_EOF = None
+        kafka.KafkaException = NotImplementedError
+        kafka.Consumer = ConsumerMock
